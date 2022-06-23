f020a411ff27bd713b1c59db5f26fad3ae7d36f8
Matheus H G Santos <matheus.beoulve@outlook.com.br>
2022-06-23 02:37:13 +0000

    Initial commit

---
+++ src/main/java/com/github/matheusbeoulve/MicronautPicocliExampleCommand.java
@@ -0,0 +1,28 @@
+package com.github.matheusbeoulve;
+
+import io.micronaut.configuration.picocli.PicocliRunner;
+import io.micronaut.context.ApplicationContext;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "micronaut-picocli-example", description = "...",
+        mixinStandardHelpOptions = true)
+public class MicronautPicocliExampleCommand implements Runnable {
+
+    @Option(names = {"-v", "--verbose"}, description = "...")
+    boolean verbose;
+
+    public static void main(String[] args) throws Exception {
+        PicocliRunner.run(MicronautPicocliExampleCommand.class, args);
+    }
+
+    public void run() {
+        // business logic here
+        if (verbose) {
+            System.out.println("Hi!");
+        }
+    }
+}
